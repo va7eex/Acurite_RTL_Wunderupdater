576e0af2d10d5637e0c002fa18b437107817f094
va7eex <david@va7eex.ca>
2017-03-16 02:15:10 -0700

    Update wx.sh

--- wx.sh
+++ wx.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-ppm=66
-wxdevice=9
-timer=570
+ppm=66 #ppm measured with rtl_test
+wxdevice=9 #acurite 5n1 code
+timer=570 #how long should we listen to the radio before we submit data
 
 timestamp() {
   date +"%D %T"
 }
 
-timestamp > /var/tmp/wx.txt
+timestamp > /var/tmp/wx.txt #This timestamp is almost entirely used for debugging
 sudo timeout $timer rtl_433 -p $ppm -R $wxdevice >> /var/tmp/wx.txt
 python wx.py
